c76129265a603b2091de2fe38cdff00730034ceb
Gergo Miklos <gergo.marcell.miklos@gmail.com>
2022-05-05 02:33:39 +0200

    0.0.101

--- src/components/text-input/text-input.tsx
+++ src/components/text-input/text-input.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import 'dist/index.css'
 
 export interface TextInputProps {
     value: string;
--- src/components/text/text.tsx
+++ src/components/text/text.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import 'dist/index.css'
 
 export interface TextProps {
     value: string;
